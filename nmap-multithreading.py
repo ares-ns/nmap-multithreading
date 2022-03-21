767b15b9ac534309197963149801f8ec018c8cd0
ARES <93425979+ares-ns@users.noreply.github.com>
2022-03-21 10:14:35 +0100

    args patch

--- nmap-multithreading.py
+++ nmap-multithreading.py
@@ -3,26 +3,25 @@
 import os
 import threading
 import time
-from queue import Queue
 
 def check_root_uid():
     if os.getuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'.")
 
-def host_discovery():
+def host_discovery(ip):
     print("Thread 1:\tDiscovery host by CIDR\n")
-    os.system("nmap -sn {0} -oX /tmp/host_discovery.xml".format(cidrNotation))
+    os.system("nmap -sn {ip} -oX /tmp/host_discovery.xml")
 
-def services_scan():
+def services_scan(ip):
     print("Thread 2:\tScanning services\n")
-    os.system("nmap -sCV -A -p- {0} -oX /tmp/services.xml".format(cidrNotation))
+    os.system("nmap -sCV -A -p- {ip} -oX /tmp/services.xml")
     
 if __name__ == '__main__':
     check_root_uid()
     os.system("clear")
     cidrNotation = input(r"Insert your CIDR: ")
-    thread1 = threading.Thread(target = host_discovery)
-    thread2 = threading.Thread(target = services_scan)
+    thread1 = threading.Thread(target = host_discovery, args=(cidrNotation,))
+    thread2 = threading.Thread(target = services_scan, args=(cidrNotation,))
     # starting threads
     thread1.start()
     thread2.start()
